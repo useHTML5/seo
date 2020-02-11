2d414e072b3b3144266572e5ff99cb8e509b532f
useHTML5 <mne@sait-nado.ru>
2020-02-11 17:29:52 +0500

    fix path

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="seo",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     install_requires=[
         "django>=2.2",
         'django-solo',
-
     ],
 )
